09efa5bfd02b8e7c174f3f6a6bb5c5e6d42448bc
Kaung Zaw Thant <66521022+kztJames01@users.noreply.github.com>
2025-01-17 23:25:51 -0600

    Overhauled exec file and only left with test functionality and links need to fix

--- Sources/ApiStocksExec/ApiStocksExec.swift
+++ Sources/ApiStocksExec/ApiStocksExec.swift
@@ -8,6 +8,7 @@ import Foundation
 import ApiStocks
 @main
 struct ApiStocksExec{
+    private static let api = ApiStocks()
     static func main() async{
         let headers = [
             "x-rapidapi-key": "c5d836857cmshba68fc722dba282p1df907jsn5110b8a4e164",
@@ -20,45 +21,22 @@ struct ApiStocksExec{
         
         let chartURL = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=AAPL&interval=3mo&diffandsplits=false"
             
-        
-        if let quoteResponse = await fetchRequest(urlString: quoteURL, headers: headers, decodeType: QuoteResponse.self){
-            print("Quote Works")
-        }
-        print("            ")
-        if let searchResponse = await fetchRequest(urlString: searchURL, headers: headers, decodeType: SearchTicker.self){
-            print("Search Works")
-        }
-        
-        print("         ")
-        
-        if let chartResponse = await fetchRequest(urlString: chartURL, headers: headers, decodeType: MarketDataResponse.self){
-            print(chartResponse)
-        }
-        
-    }
-    static func fetchRequest<T: Decodable>(urlString: String, headers: [String: String], decodeType: T.Type) async -> T?{
-        
-        guard let url = URL(string: urlString) else{
-            print("Invalid URL.")
-            return nil
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.allHTTPHeaderFields = headers
-        
-        do {
-            let (data, response) = try! await URLSession.shared.data(for: request)
+        do{
             
-            if let httpResponse = response as? HTTPURLResponse, !(200...499).contains(httpResponse.statusCode){
-                print("URL not found, HTTP Error: \(httpResponse.statusCode)")
-                return nil
-            }
+            let history = try? await api
+                .fetchHistoryData(symbol: "AAPL", interval: "1mo", diffandsplits: false)
+            print(history ?? "Not Found")
             
-            return try JSONDecoder().decode(decodeType, from: data)
-        } catch{
-            print("An error occured: \(error.localizedDescription)")
-            return nil
+            let searchTickers = try? await api
+                .searchData(query: "AA")
+            print(searchTickers ?? [])
+            
+            let quote = try? await api
+                .fetchQuotes(symbol: "AAPL")
+            print(quote ?? [])
+        } catch {
+            print(error.localizedDescription)
         }
     }
+    
 }
