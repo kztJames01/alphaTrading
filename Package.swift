0d48f92ae70b36d9ca4da2d8281e317a6b208daa
Kaung Zaw Thant <66521022+kztJames01@users.noreply.github.com>
2025-01-25 03:00:38 -0600

    Package Update

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         
         .target(
-            name: "ApiStocks"
+            name: "AlphaTrading",
+            dependencies: ["ApiStocks"]
             ),
         
         .executableTarget(name: "ApiStocksExec", dependencies: ["ApiStocks"]),
